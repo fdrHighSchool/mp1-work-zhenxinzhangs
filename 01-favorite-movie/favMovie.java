c8afe3ae996cb4a26e35f6da5e111c28c5ae129f
AP Java Period 5 <apjavaperiod5@ac1mwk18gjc9h.78b505.nycboe.org>
2022-10-19 11:46:15 -0400

    bulk add

---
+++ 01-favorite-movie/favMovie.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class favMovie here.
+ *
+ * @ Zhen 
+ * @ 9/21/22
+ */
+public class favMovie
+{
+     public static void main(String[]args){
+         System.out.println("I love the movie Avengers Endgame");
+         System.out.println("I love the movie The Nut Job");
+     }
+}
---
+++ 05-rng/random.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class random here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public static String generatePassword(int length){
+     String password;
+     
+     
+     
+     return password;
+ }
